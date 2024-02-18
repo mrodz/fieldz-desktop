c96764590be2a4b795bf669768c9b422dc34f558
mrodz <79176075+mrodz@users.noreply.github.com>
2024-02-18 14:09:50 -0800

    conflict resolution

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -22,6 +22,25 @@ pub struct League {
     regions: Vec<Pin<Box<Region>>>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ScheduledOutput {
+    valid_games: HashSet<Reservation>,
+    unable_to_schedule: Vec<Reservation>,
+    unplayed_matches: Vec<Game>,
+}
+
+impl ScheduledOutput {
+    pub fn valid_games(&self) -> &HashSet<Reservation> {
+        &self.valid_games
+    }
+    pub fn unable_to_schedule(&self) -> &[Reservation] {
+        &self.unable_to_schedule
+    }
+    pub fn unplayed_matches(&self) -> &[Game] {
+        &self.unplayed_matches
+    }
+}
+
 impl League {
     pub fn new() -> Self {
         Self { regions: vec![] }
@@ -31,8 +50,12 @@ impl League {
         self.regions.push(Box::pin(region));
     }
 
-    pub fn schedule<'a>(&'a mut self) -> HashSet<Reservation> {
+    pub fn schedule(&mut self) -> Result<ScheduledOutput> {
         let mut set = HashSet::new();
+
+        let mut unable_to_schedule = Vec::new();
+
+        let mut unplayed_matches: Vec<Game> = Vec::new();
 
         for region in self.regions.iter_mut() {
             let mut graph = region.season_graph();
@@ -40,60 +63,179 @@ impl League {
 
             let mut next_window = time_queue.next();
 
-            let mut busy: HashMap<Pin<&Team>, AvailabilityWindow> = HashMap::new();
+            let mut busy: HashMap<Pin<&Team>, Vec<&AvailabilityWindow>> = HashMap::new();
             let iter_access_point: *mut GraphMap<_, _, _> = &mut graph as *mut _;
+
             let mut edges = graph.all_edges();
 
-            while let (Some((home, away, edge)), Some(next_window_view)) =
+
+            while let (Some((mut home, mut away, edge)), Some(next_window_view)) =
                 (edges.next(), next_window)
             {
+                println!("\n-- BEGIN TRANSACTION\n{home}, {away}, {edge:?}");
                 if edge.is_some() {
+                    println!("\talready set\n--END TRANSACTION");
                     // we can ignore this edge, since it was already set
                     continue;
                 }
 
-                busy.retain(|_, window| {
+                let collision_fn = |window: &&AvailabilityWindow| {
                     AvailabilityWindow::overlap(
+                        (*window).clone(),
                         next_window_view.availability_window.clone(),
-                        window.clone(),
                         TimeDelta::new(0, 0).unwrap(),
                     )
-                    .expect(">:(")
-                });
+                    .unwrap()
+                };
 
-                if busy.contains_key(&home) || busy.contains_key(&away) {
-                    continue;
-                } else {
-                    let game = Game::new(home.deref(), away.deref());
-                    next_window_view.set_game(game);
+                if let Some(home_times) = busy.get(&home) {
+                    if home_times.iter().any(collision_fn) {
+                        println!(
+                            "\t{} <home> is busy at {}, finding more opponents:",
+                            home, next_window_view.availability_window
+                        );
+                        let other_matches = graph.edges(away);
+                        let mut unschedulable = true;
+                        for (_, new_home, reservation) in other_matches {
+                            if let Some(reservation) = reservation {
+                                println!(
+                                    "\t\t{} v {} was already scheduled ({reservation})",
+                                    new_home, away
+                                );
+                                continue;
+                            }
 
-                    /*
-                     * This is actually safe behavior, because we want our edits to be present
-                     * in future iterations.
-                     *
-                     * Rust does not know that it is safe to mutate the edge's weight (because the
-                     * iterator is borrowed immutably), so we use a pointer to bypass the borrow checker.
-                     */
-                    unsafe {
-                        (*iter_access_point).update_edge(home, away, Some(next_window_view));
+                            if busy
+                                .get(&new_home)
+                                .is_some_and(|times| times.iter().any(collision_fn))
+                            {
+                                println!(
+                                    "\t\t{} cannot play as home because they're booked at {}",
+                                    new_home, next_window_view.availability_window
+                                );
+                                continue;
+                            }
+
+                            home = new_home;
+                            unschedulable = false;
+                            println!("\t\t\tFound new home team! {home}");
+                            break;
+                        }
+
+                        if unschedulable {
+                            println!("\tWasting space!");
+                            // unable_to_schedule.push(next_window_view.clone());
+                            // next_window = time_queue.next();
+                            continue;
+                        }
                     }
-                    next_window = time_queue.next();
                 }
+
+                if let Some(away_times) = busy.get(&away) {
+                    if away_times.iter().any(collision_fn) {
+                        println!(
+                            "\t{} <away> is busy at {}, finding more opponents:",
+                            away, next_window_view.availability_window
+                        );
+                        let other_matches = graph.edges(home);
+                        let mut unschedulable = true;
+                        for (_, new_away, reservation) in other_matches {
+                            if let Some(reservation) = reservation {
+                                println!(
+                                    "\t\t{} v {} was already scheduled ({reservation})",
+                                    home, new_away
+                                );
+                                continue;
+                            }
+
+                            if busy
+                                .get(&new_away)
+                                .is_some_and(|times| times.iter().any(collision_fn))
+                            {
+                                println!(
+                                    "\t\t{} cannot play as away because they're booked at {}",
+                                    new_away, next_window_view.availability_window
+                                );
+                                continue;
+                            }
+
+                            away = new_away;
+                            unschedulable = false;
+                            println!("\t\tFound new away team! {away}");
+                            break;
+                        }
+
+                        if unschedulable {
+                            println!("\tWasting space!\n--END TRANSACTION");
+                            unable_to_schedule.push(next_window_view.clone());
+                            next_window = time_queue.next();
+                            continue;
+                        }
+                    }
+                }
+
+                let game = Game::new(home.deref(), away.deref());
+                next_window_view.set_game(game);
+
+                println!("\tLet's go! {next_window_view}");
+
+                /*
+                 * This is actually safe behavior, because we want our edits to be present
+                 * in future iterations.
+                 *
+                 * Rust does not know that it is safe to mutate the edge's weight (because the
+                 * iterator is borrowed immutably), so we use a pointer to bypass the borrow checker.
+                 */
+                unsafe {
+                    (*iter_access_point).update_edge(home, away, Some(next_window_view));
+                }
+
+                busy.entry(home)
+                    .and_modify(|v| v.push(&next_window_view.availability_window))
+                    .or_insert(vec![&next_window_view.availability_window]);
+                busy.entry(away)
+                    .and_modify(|v| v.push(&next_window_view.availability_window))
+                    .or_insert(vec![&next_window_view.availability_window]);
+
+                next_window = time_queue.next();
+
+                println!("-- END TRANSACTION")
             }
 
-            set.extend(graph.all_edges().filter_map(|x| x.2.cloned()))
+            set.extend(graph.all_edges().filter_map(|(.., r)| r.cloned()));
+            unable_to_schedule.append(&mut time_queue.map(Reservation::clone).collect());
+
+            // if we exit early, we have to check if the last window was fulfilled.
+            if next_window.is_some_and(Reservation::is_available) {
+                unable_to_schedule.push(next_window.unwrap().clone())
+            }
+
+            unplayed_matches.append(&mut graph.all_edges().filter_map(|(home, away, reservation)| {
+                if reservation.is_none() {
+                    Some(Game::new(home.deref(), away.deref()))
+                } else {
+                    None
+                }
+            }).collect());
         }
 
-        set
+        let result = ScheduledOutput {
+            valid_games: set,
+            unable_to_schedule,
+            unplayed_matches
+        };
+
+        Ok(result)
     }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Region {
-    fields: Vec<Field>,
+    fields: Vec<Pin<Box<Field>>>,
     teams: Vec<Pin<Box<Team>>>,
 }
 
+#[derive(Clone)]
 pub struct RegionalGameQueue<'a> {
     region: &'a Region,
     index: (usize, usize),
@@ -146,11 +288,10 @@ impl Region {
     }
 
     pub fn add_team(&mut self, identifier: impl Into<String>) {
-        let self_ref = self as *mut _;
-
+        let region = NonNull::from(self.deref());
         self.teams.push(Box::pin(Team {
             identifier: identifier.into(),
-            region: NonNull::new(self_ref).unwrap(),
+            region,
         }))
     }
 
@@ -160,13 +301,27 @@ impl Region {
         availability: &[AvailabilityWindow],
     ) {
         let region = NonNull::new(self as *mut _).unwrap();
-        let matches = VecDeque::from_iter(availability.iter().map(Reservation::new));
 
-        self.fields.push(Field {
+        let matches = VecDeque::from_iter(
+            availability
+                .iter()
+                .map(|w| Reservation::new(w.clone(), NonNull::dangling())),
+        );
+
+        let mut pinned_field = Box::pin(Field {
             identifier: identifier.into(),
             region,
             matches,
-        })
+        });
+
+        let cyclical_self = NonNull::from(pinned_field.deref());
+
+        pinned_field
+            .matches
+            .iter_mut()
+            .for_each(|r| r.field = cyclical_self);
+
+        self.fields.push(pinned_field)
     }
 
     pub fn season_graph(&self) -> GraphMap<Pin<&Team>, Option<&Reservation>, Directed> {
@@ -186,7 +341,7 @@ impl Region {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Team {
     identifier: String,
     region: NonNull<Region>,
@@ -201,7 +356,13 @@ impl Display for Team {
 #[macro_export]
 macro_rules! window {
     ($dd:literal/$mm:literal/$yy:literal from $start_h:literal:$start_m:literal to $end_h:literal:$end_m:literal) => {
-        AvailabilityWindow::single_day($dd, $mm, $yy, ($start_h, $start_m), ($end_h, $end_m))
+        $crate::AvailabilityWindow::single_day(
+            $dd,
+            $mm,
+            $yy,
+            ($start_h, $start_m),
+            ($end_h, $end_m),
+        )
     };
 }
 
@@ -240,12 +401,12 @@ impl AvailabilityWindow {
         end: (u32, u32),
     ) -> Result<Self> {
         let start = Local
-            .with_ymd_and_hms(year.try_into()?, month, day, start.0, start.1, 0)
-            .single()
+            .with_ymd_and_hms(year.try_into()?, dbg!(month), dbg!(day), dbg!(start.0), dbg!(start.1), 0)
+            .earliest()
             .context("ambiguous start date")?;
         let end = Local
             .with_ymd_and_hms(year.try_into()?, month, day, end.0, end.1, 0)
-            .single()
+            .earliest()
             .context("ambiguous end date")?;
 
         if end < start {
@@ -294,6 +455,12 @@ pub struct Field {
     matches: VecDeque<Reservation>,
 }
 
+impl Display for Field {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.identifier)
+    }
+}
+
 impl PartialOrd for Field {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.identifier.partial_cmp(&other.identifier)
@@ -318,17 +485,20 @@ impl Eq for Field {}
 pub struct Reservation {
     availability_window: AvailabilityWindow,
     game: Cell<Option<Game>>,
+    field: NonNull<Field>,
 }
 
 impl Display for Reservation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(game) = self.game.get() {
-            write!(f, "{game}")?;
-        } else {
-            write!(f, "<empty time slot>")?;
+        unsafe {
+            write!(f, "{} [{}]", self.availability_window, self.field.as_ref())?;
         }
 
-        write!(f, " at {}", self.availability_window)
+        if let Some(game) = self.game.get() {
+            write!(f, " {game}")
+        } else {
+            write!(f, " <unused>")
+        }
     }
 }
 
@@ -340,10 +510,11 @@ impl Hash for Reservation {
 }
 
 impl Reservation {
-    pub fn new(availability_window: &AvailabilityWindow) -> Self {
+    pub const fn new(availability_window: AvailabilityWindow, field: NonNull<Field>) -> Self {
         Self {
-            availability_window: availability_window.to_owned(),
+            availability_window,
             game: Cell::new(None),
+            field,
         }
     }
 
@@ -361,6 +532,10 @@ impl Reservation {
 
     pub fn is_available(&self) -> bool {
         self.game.get().is_none()
+    }
+
+    pub fn time(&self) -> &AvailabilityWindow {
+        &self.availability_window
     }
 }
 
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 
 use anyhow::Result;
-use backend::{AvailabilityWindow, League, Region};
+use backend::{window, League, Region, RegionalGameQueue};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -15,33 +15,60 @@ fn main() -> Result<()> {
     r1.add_field(
         "Big Blue Park",
         &[
-            backend::window!(9/11/2006 from 12:30 to 14:00)?,
-            backend::window!(9/12/2006 from 12:30 to 14:00)?,
-            backend::window!(9/12/2006 from 14:30 to 16:00)?,
-            backend::window!(9/12/2006 from 14:30 to 16:00)?,
-            backend::window!(9/12/2006 from 14:30 to 16:00)?,
-            backend::window!(9/12/2006 from 14:30 to 16:00)?,
-            backend::window!(9/12/2006 from 14:30 to 16:00)?,
+            window!(11/9/2006 from 9:30 to 11:00)?,
+            window!(11/9/2006 from 11:30 to 13:00)?,
+            window!(12/9/2006 from 13:30 to 15:00)?,
+            window!(12/9/2006 from 8:00 to 9:30)?,
+            window!(12/9/2006 from 10:00 to 11:30)?,
+            window!(13/9/2006 from 12:00 to 13:00)?,
+            window!(14/9/2006 from 8:00 to 9:30)?,
+            window!(14/9/2006 from 14:00 to 15:30)?,
+        ],
+    );
+
+    r1.add_field(
+        "Field Two",
+        &[
+            window!(11/9/2006 from 9:30 to 11:00)?,
+            window!(11/9/2006 from 11:30 to 13:00)?,
+            window!(11/9/2006 from 13:30 to 15:00)?,
+            window!(12/9/2006 from 13:30 to 15:00)?,
         ],
     );
 
     r1.add_team("Rockies");
     r1.add_team("Purple Dragons");
     r1.add_team("Green Machine");
+    r1.add_team("Red Rubies");
 
-    // for a in RegionalGameQueue::new(&r1) {
-    //     println!("{a}")
-    // }
+    println!("Providing times:");
+    for time in RegionalGameQueue::new(&r1) {
+        println!("- {time}");
+    }
+    println!();
 
     league.add_region(r1);
 
-    let games = league.schedule();
+    let out = league.schedule()?;
 
+    println!("\nValid Games");
+
+    let mut games = out.valid_games().iter().collect::<Vec<_>>();
+    games.sort_by(|x, y| x.time().cmp(y.time()));
+        
     for game in games {
         println!("{game}")
     }
 
-    // dbg!(league);
+    println!("\nCould not use these times because of conflicts");
+    for unused in out.unable_to_schedule() {
+        println!("- {unused}")
+    }
+
+    println!("\nUnscheduled matches");
+    for unused in out.unplayed_matches() {
+        println!("- {unused}")
+    }
 
     Ok(())
 }
