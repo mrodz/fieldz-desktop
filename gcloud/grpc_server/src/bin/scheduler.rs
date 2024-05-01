059135dde1a3ee5c4985de8fd17eec797e472e75
mrodz <79176075+mrodz@users.noreply.github.com>
2024-04-30 18:19:32 -0700

    add health server

--- gcloud/grpc_server/src/bin/scheduler.rs
+++ gcloud/grpc_server/src/bin/scheduler.rs
@@ -5,7 +5,7 @@ use tonic::transport::Server;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
-async fn get_address() -> Result<SocketAddr, Box<dyn std::error::Error>> {
+fn get_address() -> Result<SocketAddr, Box<dyn std::error::Error>> {
     if cfg!(feature = "gcp") {
         "[::1]:8080"
     } else {
@@ -21,12 +21,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     std::env::var("QUOTA_SERVER_URL").map_err(Box::new)?;
 
-    let addr = get_address().await?;
+    let addr = get_address()?;
 
     let subscriber = FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
         .with_max_level(Level::INFO)
+        .with_ansi(false)
         // completes the builder.
         .finish();
 
@@ -34,11 +35,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing::info!("Hosting gRPC server on: {addr}");
 
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+
+    health_reporter
+        .set_serving::<SchedulerServer<ScheduleManager>>()
+        .await;
+
     let scheduler = SchedulerServer::new(ScheduleManager);
 
     tracing::info!("Using server: {scheduler:?}");
+    tracing::info!("Health check: {health_reporter:?}");
 
-    Server::builder().add_service(scheduler).serve(addr).await?;
+    Server::builder()
+        .add_service(health_service)
+        .add_service(scheduler)
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
